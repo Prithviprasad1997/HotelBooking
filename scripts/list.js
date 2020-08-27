aa62363d26eeaea79b866f39434ce172c104713e
Prithviprasad1997 <prithvi97prasad@gmail.com>
2020-08-28 00:02:55 +0530

    Checkpoint 8

---
+++ scripts/list.js
@@ -0,0 +1,11 @@
+function myFunction() {
+    var x = document.getElementById("mapView");
+    var y = document.getElementById("sc1")
+    if (x.style.display === "none") {
+        x.style.display = "block";
+        y.style.display = "none";
+    } else {
+        x.style.display = "none";
+        y.style.display = "flex";
+    }
+}
